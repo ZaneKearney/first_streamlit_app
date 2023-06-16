6d9ab3d18e5291f1d406cf17e4865fd43bb5de2d
Zane Kearney <zanekearney@gmail.com>
2023-06-16 10:47:16 +0800

    updated picklist index

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parents New Diner")
 
@@ -10,8 +11,8 @@ streamlit.text('🥑🍞 Avocado on toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))

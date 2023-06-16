a05029e4d8df764b7fb3313981c201473287343e
Zane Kearney <zanekearney@gmail.com>
2023-06-16 10:43:37 +0800

    restarted

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-import pandas
 
 streamlit.title("My Parents New Diner")
+
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
@@ -10,10 +10,6 @@ streamlit.text('🥑🍞 Avocado on toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-
-#Pick list for users to select the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list))
-
 streamlit.dataframe(my_fruit_list)
